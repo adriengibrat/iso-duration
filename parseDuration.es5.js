1b481a64ea6191930d1b4e0edc0d79a9c5406455
Adrien Gibrat <adrien.gibrat@gmail.com>
2016-08-26 21:46:24 +0200

    change add method logic, add addUTC method

--- parseDuration.es5.js
+++ parseDuration.es5.js
@@ -1,42 +1,64 @@
-(function (name, definition) {
-	if (typeof module != 'undefined') module.exports = definition();
-	else if (typeof define == 'function' && typeof define.amd == 'object') define(name, [], definition);
-	else this[name] = definition();
-}('parseDuration', function () {
-	/**
-	 * Parse ISO 8601 duration (with a few limitations)
-	 *
-	 * @see https://en.wikipedia.org/wiki/ISO_8601#Durations
-	 *
-	 * @example
-	 * var aDayAgo = parseDuration('-P1D').add, yesterday = aDayAgo(new Date());
-	 *
-	 * @param {String} duration: ISO 8601 duration, only in "PnYnMnDTnHnMnS" or "PnW" formats, n being an integer
-	 * @throws {Error} When duration cannot be parsed
-	 * @returns {Object} Parsed duration with "add" method that sums or substracts parsed duration to a given date, accorging duration sign
-	 */
-	var durationRegex = /^(-)?P(?:(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?|(\d+)W)$/;
-	return function parseDuration (duration) {
-		var parsed;
-		duration && duration.replace(durationRegex, function (_, sign, year, month, day, hour, minute, second, week) {
-			sign = sign ? -1 : 1;
-			// parse number for each unit
-			var units = [year, month, day, hour, minute, second, week].map(function (num) { return parseInt(num, 10) * sign || 0; });
-			parsed = {year: units[0], month: units[1], week: units[6], day: units[2], hour: units[3], minute: units[4], second: units[5]};
-		});
-		// no regexp match
-		if (!parsed) { throw new Error('Invalid duration "' + duration + '"'); }
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+	typeof define === 'function' && define.amd ? define('parseDuration', factory) :
+	(global.parseDuration = factory());
+}(this, (function () { 'use strict';
+
+/**
+ * Parse ISO 8601 duration (with few limitations)
+ *
+ * @see https://en.wikipedia.org/wiki/ISO_8601#Durations ISO 8601 duration explained
+ *
+ * @see https://gist.github.com/adriengibrat/e0b6d16cdd8c584392d8 Previous history
+ *
+ * @example
+ * const aDayAgo = parseDuration('-P1D').add, yesterday = aDayAgo(new Date())
+ *
+ * @param {String} duration: ISO 8601 duration, only in "PnYnMnDTnHnMnS" or "PnW" formats, n being an integer
+ * @throws {Error} When duration cannot be parsed
+ * @returns {Object} Parsed duration with "add" method that sums or substracts parsed duration to a given date, accorging duration sign
+ */
+
+var checkDate = function (date) {
+	if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.valueOf()))
+		throw new TypeError('Invalide date')
+}
+
+var map = function (mapper, object) {
+	var target = {}
+	for (var key in object)
+		if (object.hasOwnProperty(key))
+			target[key] = mapper(object[key], key, object)
+	return target
+}
+
+var durationRegex = /^(-)?P(?:(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?|(\d+)W)$/
+
+function parseDuration (duration) {
+	var parsed
+
+	if (duration)
+		duration.replace(durationRegex, function (_, sign, year, month, day, hour, minute, second, week) {
+			sign = sign ? -1 : 1
+			var toNumber = function (num) { return parseInt(num, 10) * sign || 0; }
+			parsed = map(toNumber, { year: year, month: month, day: day, hour: hour, minute: minute, second: second, week: week })
+		})
+
+	if (!parsed)
+		throw new Error(("Invalid duration \"" + duration + "\""))
+
+	return Object.assign(parsed, {
 		/**
-		 * Sum or substract parsed duration to date
+		 * Sum or substract parsed duration to given date using UTC logic
+		 * Time in the day may be affected by daylight saving time (DST)
+		 * Time interval between the date given and returned will be stricly equal to duration
 		 *
-		 * @param {Date} date: A valid date instance
+		 * @param {Date} date: Any valid date
 		 * @throws {TypeError} When date is not valid
-		 * @returns {Date} Date plus or minus duration, according duration sign
+		 * @returns {Date} New date with duration difference
 		 */
-		parsed.add = function add (date) {
-			if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.valueOf())) {
-				throw new TypeError('Invalide date');
-			}
+		addUTC: function addUTC(date) {
+			checkDate(date);
 			return new Date(Date.UTC(
 				date.getUTCFullYear() + parsed.year,
 				date.getUTCMonth() + parsed.month,
@@ -45,8 +67,33 @@
 				date.getUTCMinutes() + parsed.minute,
 				date.getUTCSeconds() + parsed.second,
 				date.getUTCMilliseconds()
-			));
-		};
-		return parsed;
-	};
-}));
+			))
+		},
+
+		/**
+		 * Sum or substract parsed duration to date
+		 * Time in the day won't be affected by daylight saving time (DST)
+		 * Time interval between the date given and returned may be affected by DST and not equal to duration
+		 *
+		 * @param {Date} date: Any valid date
+		 * @throws {TypeError} When date is not valid
+		 * @returns {Date} New date with duration difference
+		 */
+		add: function add(date) {
+			checkDate(date);
+			return new Date(
+				date.getFullYear() + parsed.year,
+				date.getMonth() + parsed.month,
+				date.getDate() + parsed.day + parsed.week * 7,
+				date.getHours() + parsed.hour,
+				date.getMinutes() + parsed.minute,
+				date.getSeconds() + parsed.second,
+				date.getMilliseconds()
+			)
+		}
+	})
+}
+
+return parseDuration;
+
+})));
--- parseDuration.es6.js
+++ parseDuration.es6.js
@@ -1,39 +1,58 @@
 /**
- * Parse ISO 8601 duration (with a few limitations)
+ * Parse ISO 8601 duration (with few limitations)
  *
- * @see https://en.wikipedia.org/wiki/ISO_8601#Durations
+ * @see https://en.wikipedia.org/wiki/ISO_8601#Durations ISO 8601 duration explained
+ *
+ * @see https://gist.github.com/adriengibrat/e0b6d16cdd8c584392d8 Previous history
  *
  * @example
- * let aDayAgo = parseDuration('-P1D').add, yesterday = aDayAgo(new Date());
+ * const aDayAgo = parseDuration('-P1D').add, yesterday = aDayAgo(new Date())
  *
  * @param {String} duration: ISO 8601 duration, only in "PnYnMnDTnHnMnS" or "PnW" formats, n being an integer
  * @throws {Error} When duration cannot be parsed
  * @returns {Object} Parsed duration with "add" method that sums or substracts parsed duration to a given date, accorging duration sign
  */
-let durationRegex = /^(-)?P(?:(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?|(\d+)W)$/;
+
+const checkDate = date => {
+	if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.valueOf()))
+		throw new TypeError('Invalide date')
+}
+
+const map = (mapper, object) => {
+	const target = {}
+	for (let key in object)
+		if (object.hasOwnProperty(key))
+			target[key] = mapper(object[key], key, object)
+	return target
+}
+
+const durationRegex = /^(-)?P(?:(?:(\d+)Y)?(?:(\d+)M)?(?:(\d+)D)?(?:T(?:(\d+)H)?(?:(\d+)M)?(?:(\d+)S)?)?|(\d+)W)$/
+
 export default function parseDuration (duration) {
-	let parsed;
-	duration && duration.replace(durationRegex, (_, sign, ...units) => {
-		sign = sign ? -1 : 1;
-		// parse number for each unit
-		let [year, month, day, hour, minute, second, week] = units.map((num) => parseInt(num, 10) * sign || 0);
-		parsed = {year, month, week, day, hour, minute, second};
-	});
-	// no regexp match
-	if (!parsed) { throw new Error(`Invalid duration "${duration}"`); }
+	let parsed
+
+	if (duration)
+		duration.replace(durationRegex, (_, sign, year, month, day, hour, minute, second, week) => {
+			sign = sign ? -1 : 1
+			const toNumber = num => parseInt(num, 10) * sign || 0
+			parsed = map(toNumber, { year, month, day, hour, minute, second, week })
+		})
+
+	if (!parsed)
+		throw new Error(`Invalid duration "${duration}"`)
 
 	return Object.assign(parsed, {
 		/**
-		 * Sum or substract parsed duration to date
+		 * Sum or substract parsed duration to given date using UTC logic
+		 * Time in the day may be affected by daylight saving time (DST)
+		 * Time interval between the date given and returned will be stricly equal to duration
 		 *
 		 * @param {Date} date: Any valid date
 		 * @throws {TypeError} When date is not valid
 		 * @returns {Date} New date with duration difference
 		 */
-		add(date) {
-			if (Object.prototype.toString.call(date) !== '[object Date]' || isNaN(date.valueOf())) {
-				throw new TypeError('Invalide date');
-			}
+		addUTC(date) {
+			checkDate(date);
 			return new Date(Date.UTC(
 				date.getUTCFullYear() + parsed.year,
 				date.getUTCMonth() + parsed.month,
@@ -42,7 +61,29 @@ export default function parseDuration (duration) {
 				date.getUTCMinutes() + parsed.minute,
 				date.getUTCSeconds() + parsed.second,
 				date.getUTCMilliseconds()
-			));
+			))
+		},
+
+		/**
+		 * Sum or substract parsed duration to date
+		 * Time in the day won't be affected by daylight saving time (DST)
+		 * Time interval between the date given and returned may be affected by DST and not equal to duration
+		 *
+		 * @param {Date} date: Any valid date
+		 * @throws {TypeError} When date is not valid
+		 * @returns {Date} New date with duration difference
+		 */
+		add(date) {
+			checkDate(date);
+			return new Date(
+				date.getFullYear() + parsed.year,
+				date.getMonth() + parsed.month,
+				date.getDate() + parsed.day + parsed.week * 7,
+				date.getHours() + parsed.hour,
+				date.getMinutes() + parsed.minute,
+				date.getSeconds() + parsed.second,
+				date.getMilliseconds()
+			)
 		}
-	});
+	})
 }
